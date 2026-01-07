b5957a1d794eb89e3d273d5daa9b41a41f7d882d
Meo <miaopf@dkingsoft.com>
2026-01-08 01:29:44 +0800

    chore(build): migrate to IntelliJ Platform Gradle Plugin 2.x
    - upgrade IntelliJ Platform Gradle Plugin from 1.14.1 to 2.1.0
    - upgrade Kotlin from 1.8.21 to 2.1.0
    - migrate build configuration to 2.x DSL syntax
    - update target IDE from 2022.2.5 to 2023.3.8
    - expand plugin compatibility to support IntelliJ IDEA 2022.2 - 2025.3
    - update Gradle wrapper to 8.5 using Tencent Cloud mirror
    - migrate kotlinOptions to compilerOptions DSL
    - add plugin verification for multiple IDE versions
    - remove obsolete Kotlin 1.8.20 workaround
    
    Refs: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-migration.html

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,33 +1,48 @@
 plugins {
     id("java")
-    id("org.jetbrains.kotlin.jvm") version "1.8.21"
-    id("org.jetbrains.intellij") version "1.14.1"
+    id("org.jetbrains.kotlin.jvm") version "2.1.0"
+    id("org.jetbrains.intellij.platform") version "2.1.0"
 }
 
 group = "com.meo"
 version = "0.1.2"
 
 repositories {
-    mavenLocal()
     mavenCentral()
+    intellijPlatform {
+        defaultRepositories()
+    }
 }
 
 java {
     sourceCompatibility = JavaVersion.VERSION_17
 }
 
-// Configure Gradle IntelliJ Plugin
-// Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
-intellij {
-    version.set("2022.2.5")
-    type.set("IC") // Target IDE Platform
-    plugins.set(listOf(
-            "com.intellij.java"
-    ))
+dependencies {
+    // SQL formatter library
+    implementation("com.github.vertical-blank:sql-formatter:2.0.4")
+
+    intellijPlatform {
+        // Specify the IntelliJ IDE version used for building and running the plugin
+        intellijIdeaCommunity("2023.3.8")
+
+        // Bundled plugins
+        bundledPlugin("com.intellij.java")
+
+        // Plugin verifier
+        pluginVerifier()
+        zipSigner()
+        instrumentationTools()
+    }
 }
 
-dependencies {
-    implementation ("com.github.vertical-blank:sql-formatter:2.0.4")
+intellijPlatform {
+    pluginVerification {
+        ides {
+            ide("IC-2023.3.8")
+            ide("IC-2022.2.5")
+        }
+    }
 }
 
 tasks {
@@ -37,20 +52,25 @@ tasks {
         targetCompatibility = "17"
     }
     withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions.jvmTarget = "17"
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+        }
     }
 
+    // Patch plugin.xml with build information
     patchPluginXml {
         sinceBuild.set("222")
-        untilBuild.set("232.*")
+        untilBuild.set("253.*")
     }
 
+    // Sign the plugin
     signPlugin {
         certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
         privateKey.set(System.getenv("PRIVATE_KEY"))
         password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
     }
 
+    // Publish the plugin
     publishPlugin {
         token.set(System.getenv("PUBLISH_TOKEN"))
     }
